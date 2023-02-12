533616041a2e5fb649e7c58e869260b3fcf91f9f
srikanth <d.srikanth.rover@gmail.com>
2023-02-12 17:26:06 +0530

    second commit

--- src/test/java/com/cicd/practice/CICDPractice/TEST.java
+++ src/test/java/com/cicd/practice/CICDPractice/TEST.java
@@ -1,7 +1,10 @@
 package com.cicd.practice.CICDPractice;
 
+import org.testng.annotations.Test;
+
 public class TEST {
 	
+	@Test
 	public void testMethod1() {
 		
 		System.out.println("this is just to test method1");
