b44072b22fbde4f76c2ab2c5dda2571664d9b045
srikanth <d.srikanth.rover@gmail.com>
2023-02-12 16:32:10 +0530

    first commit

---
+++ src/main/java/com/cicd/practice/CICDPractice/App.java
@@ -0,0 +1,13 @@
+package com.cicd.practice.CICDPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/cicd/practice/CICDPractice/TEST.java
@@ -0,0 +1,10 @@
+package com.cicd.practice.CICDPractice;
+
+public class TEST {
+	
+	public void testMethod1() {
+		
+		System.out.println("this is just to test method1");
+	}
+
+}
